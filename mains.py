1158d8f022ece6a9962242eba115cee908bfe070
Krishna-45Jaiswal <127404814+Krishna-45Jaiswal@users.noreply.github.com>
2023-03-16 19:54:13 +0530

    Create mains.py

---
+++ mains.py
@@ -0,0 +1,19 @@
+from config import config
+from pyrogram import Client, filters
+
+app = Client(
+    api_id=config.API_ID, api_hash=config.API_HASH, session_name=str(config.SESSION)
+)
+
+calls = PyTgCalls(app)
+
+if config.BOT_TOKEN:
+    bot = Client(
+        "Music",
+        api_id=config.API_ID,
+        api_hash=config.API_HASH,
+        bot_token=config.BOT_TOKEN,
+    )
+    client = bot
+else:
+    client = app

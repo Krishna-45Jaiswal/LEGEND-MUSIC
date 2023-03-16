cabe0e95429696848b270e2c22f2435d8f314e5e
Krishna-45Jaiswal <127404814+Krishna-45Jaiswal@users.noreply.github.com>
2023-03-16 19:59:56 +0530

    Update mains.py

--- mains.py
+++ mains.py
@@ -1,6 +1,6 @@
 from config import config
 from pyrogram import Client, filters
-
+from pytgcalls import PyTgCalls
 app = Client(
     api_id=config.API_ID, api_hash=config.API_HASH, session_name=str(config.SESSION)
 )

a06a163936aa40fff2262c116667c28962a64f81
Krishna-45Jaiswal <127404814+Krishna-45Jaiswal@users.noreply.github.com>
2023-03-16 18:02:50 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,10 +1,3 @@
-echo ">> FETCHING UPSTREAM..."
-git clone https://github.com/LEGEND-AI/LEGEND-MUSIC /LEGEND-MUSIC
-echo ">> INSTALLING REQUIREMENTS..."
-cd /LEGEND-MUSIC
-pip3 install -U -r requirements.txt
-echo ">> STARTING MUSIC PLAYER USERBOT..."
-clear
 python3 runfile.py
 echo "
 #     #                        
